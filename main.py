6b40935f7ef98a1b0fa5971eec87ba2b8199a7fa
ParkHyungkeun <njjj0117@gmail.com>
2024-10-20 14:57:42 +0900

    Update main.py
    함수확인을 위한 보조 명령문 삭제

--- main.py
+++ main.py
@@ -4,6 +4,5 @@ def twoSum(nums: List[int], target: int) -> List[int]:
         for j in range(i+1,len(nums)):
             if nums[i]+nums[j] == target:
                 return[i, j]
-print(twoSum(nums,target))
 
             
